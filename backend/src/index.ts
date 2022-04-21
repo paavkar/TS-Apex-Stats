374c24e66f126cb56eee5a6266650a13c022a7b2
paavkar <62183220+paavkar@users.noreply.github.com>
2022-04-21 19:57:26 +0300

    Winston configuration
    Configured Winston to log into a file and MongoDB. Also MongoDB connection and preparing work for MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,22 @@ import arenasRouter from './routes/arenas';
 import battleRoyaleRouter from './routes/battleRoyale';
 const app = express();
 import cors from 'cors';
+import mongoose from 'mongoose';
+import "dotenv/config";
+import logger from './logger';
+
+
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
+
+mongoose.connect(`${process.env.MONGODB_URL}`)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message);
+  });
 
 const PORT = 3001;
 
@@ -14,5 +27,5 @@ app.use('/api/battleRoyale', battleRoyaleRouter);
 app.use(express.static('build'));
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
